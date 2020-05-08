30670e3e3128e152048bcabeb41cf18a83115791
Daniel Reeves <xdanielreeves@gmail.com>
2020-05-08 18:27:01 -0400

    added version

--- flagging_site/__init__.py
+++ flagging_site/__init__.py
@@ -1,3 +1,5 @@
+__version__ = '0.0.1'
+
 # flask's CLI will automatically search for a Callable named "create_app"
 # see https://github.com/pallets/flask/blob/master/src/flask/cli.py
 from .app import create_app
