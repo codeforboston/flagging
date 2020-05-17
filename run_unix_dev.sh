b7faf6be515941834ac4d5fd7adf2fd482ead3f9
Daniel Reeves <xdanielreeves@gmail.com>
2020-05-17 15:35:11 -0400

    bugfix

--- run_unix_dev.sh
+++ run_unix_dev.sh
@@ -1,23 +1,24 @@
 function get_python_exec() {
+  # Get a Python executable that can run this app.
   pyver=`python3 -V | cut -b 8-`
   major=`echo ${pyver} | cut -d. -f1`
   minor=`echo ${pyver} | cut -d. -f2`
-  # If python3 is 3.7+, then use that.
-  # Else, attempt to use "python3.7"
+  # If python3 is 3.7+, then use that. Else, attempt to use "python3.7"
   if [ ${major} == 3 ] && [ ${minor} -ge 7 ]; then
     pyexec="python3"; else
     pyexec="python3.7"; fi
-  return ${pyexec}
+  echo ${pyexec}
 }
 
-get_python_exec
-PYEXEC=$?
+# Get proper Python executable
+PYEXEC=$(get_python_exec)
 
-PYEXEC -m venv venv
-PYEXEC -m pip install -r requirements.txt
+# Set up virtual environment in /venv
+$PYEXEC -m venv venv
+$PYEXEC -m pip install -r requirements.txt
 
+# Set up and run the Flask application
 export FLASK_APP=flagging_site
 export FLASK_ENV=development
 read -p "Enter vault password: " VAULT_PASSWORD
-
 flask run
