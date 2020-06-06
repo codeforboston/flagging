583ba8a2eb0407f6702d5fc019c4f1110ca9d9c6
dfe325 <daneder@gmail.com>
2020-06-06 07:38:46 -0400

    add setup.cfg, test files

--- flagging_site/config.py
+++ flagging_site/config.py
@@ -126,4 +126,6 @@ class TestingConfig(BaseConfig):
     """The Testing Config is used for unit-testing and integration-testing the
     website.
     """
+
+class TestingConfig(BaseConfig):
     TESTING: bool = True
---
+++ tests/test_data.py
@@ -0,0 +1,9 @@
+
+
+'''
+The unit tests for the models should be in a file called ./tests/test_data.py.
+The tests should assert that the predictions are between 0 and 1.
+Write tests in the pytest idiom.
+There are more things we can test down the line as it pertains to models and data,
+but this is a good starting place.
+'''
