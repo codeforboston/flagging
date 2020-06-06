9ad7f478c79c1c769700d9aaae0b756dedeaf671
Daniel Reeves <xdanielreeves@gmail.com>
2020-06-06 16:33:45 -0400

    bump version

--- flagging_site/__init__.py
+++ flagging_site/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.0.1'
+__version__ = '0.1.0'
 
 # flask's CLI will automatically search for a Callable named "create_app"
 # see https://github.com/pallets/flask/blob/master/src/flask/cli.py
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with io.open('README.md', 'rt', encoding='utf8') as f:
 
 setup(
     name='CRWA Flagging Website',
-    version='0.0.1',
+    version='0.1.0',
     packages=find_packages(),
     author='Code for Boston',
     python_requires='>=3.7.1',
