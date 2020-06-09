0a35e39b42431ab45ec1a689a4a11784c140df85
dfe325 <daneder@gmail.com>
2020-06-09 16:42:05 -0400

    parameterize test_site.py

--- tests/test_site.py
+++ tests/test_site.py
@@ -2,10 +2,19 @@ import pytest
 from flask import g
 from flask import session
 
-def test_home_page(client, app):
-    # test that viewing the page renders without template errors
-    assert client.get("/").status_code == 200
+'''
 
-def test_output_model(client, app):
-    # test that viewing the page renders without template errors
-    assert client.get("/output_model").status_code == 200
+Test that viewing the page renders without template errors
+'''
+@pytest.mark.parametrize('page, result',
+                         [
+                             ("/", 200),
+                             ("/output_model", 200)
+                         ]
+                         )
+def test_page(client, app, page, result):
+    '''
+    test that viewing the page renders without template errors
+    '''
+    assert client.get(page).status_code == result
+
