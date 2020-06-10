6c252082c6df3ed29324db1d61391bb0e4063e06
bxchang04 <bxchang04@gmail.com>
2020-06-10 19:53:04 -0400

    pushed code into test_data.py

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,1 +1,17 @@
-# changes 1
+#
+
+# tests/test_site.py
+
+import pytest
+from flask import g
+from flask import session
+
+
+def test_home_page(data):
+    # test that viewing the data renders the data between the values of 0 and 1
+    assert data >= 0 and data <= 1
+
+
+def test_output_model(data):
+    # test that viewing the data renders the data between the values of 0 and 1
+    assert data >= 0 and data <= 1
