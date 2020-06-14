264116df040acc493ded33e42e62c2d9d045ccce
Edwin Sun <34005715+edsun123@users.noreply.github.com>
2020-06-14 15:47:11 -0400

    added documentation and twitter folder

---
+++ flagging_site/twitter/tweet_bot.py
@@ -0,0 +1,36 @@
+"""
+This is a basic script to output a message to twitter
+User must input public and private key from Consumer
+and access. They will be imported from keys file
+
+Future Plans: 
+Allow more functionality allowing user input
+
+"""
+
+import tweepy
+from keys import *  
+
+def post_tweet(msg): 
+    """
+        Posts tweet onto twitter handle
+
+        arg: accept string message msg
+
+        returns: string message that have been inputed
+    """
+
+    # Authenticates using consumer key and secret
+    auth=tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+
+    # Authenticates using access key and secret
+    auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+    api = tweepy.API(auth)
+
+
+    #print message on terminal and update status 
+    # to twitter handle
+    print ('Tweeting' + msg) 
+    api.update_status(msg)
+
+    return msg 
