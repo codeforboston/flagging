efb7793c0d3921c063ceaaffe07a48fefcc2c874
Dan Eder <daneder@gmail.com>
2020-07-19 07:29:50 -0400

    test_data works

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,14 +1,8 @@
-import requests
+from flagging_site.data.hobolink import get_hobolink_data
+from datetime import datetime
 
 
-class TestData:
-    def test_get_hobolink_data(self):
-        data = {
-            'query': 'code_for_boston_export',         #hard-coded this value in to make test work
-            'authentication': get_keys()['hobolink']   #looked in keys.py to find this
-                                                       # tried echoing envir vars, no luck
-        }
-        hobolink_url = 'http://webservice.hobolink.com/restv2/data/custom/file'
-        res = requests.post(hobolink_url, json=data)
-        assert res.status_code == 200
-
+def test_get_hobolink_data():
+    df = get_hobolink_data('code_for_boston_export')
+    last_timestamp = df.iloc[-1][0]
+    assert (last_timestamp.minute - datetime.now().minute) < 30
