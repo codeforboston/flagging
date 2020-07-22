0951d4e550fb9f65af4136cbcbfe3e9b13479805
Dan Eder <daneder@gmail.com>
2020-07-22 08:28:53 -0400

    fix for offline mode

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,9 +1,10 @@
 from flagging_site.data.hobolink import get_hobolink_data
 import pandas as pd
 
-def test_get_hobolink_data():
-    df = get_hobolink_data('code_for_boston_export')
-    last_timestamp = df['time'].iloc[-1]
-    time_difference = (pd.to_datetime('today') - last_timestamp)
-    assert time_difference < pd.Timedelta('30 minutes')
 
+def test_get_hobolink_data(app):
+    with app.app_context():
+        df = get_hobolink_data()
+        last_timestamp = df['time'].iloc[-1]
+        time_difference = (pd.to_datetime('today') - last_timestamp)
+        assert time_difference < pd.Timedelta('30 minutes')
