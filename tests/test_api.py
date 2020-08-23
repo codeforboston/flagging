5fbb1200a23deb25c9bfc76d13315908126e0eda
Dan Eder <daneder@gmail.com>
2020-08-22 23:17:31 -0400

    initial commit

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,34 +1,5 @@
 from flask import current_app
 from flagging_site.blueprints.api import model_api
-from flagging_site.data.model import reach_2_model
-from flagging_site.data.model import process_data
-from flagging_site.data.usgs import get_live_usgs_data
-from flagging_site.data.hobolink import get_live_hobolink_data
-
-def test_model_output_api_schema(app):
-    """test_model_output_api_schema() should test that the keys are a particular way and the
-    values are of a particular type."""
-    with app.app_context():
-        schema = {
-            str: list
-            }
-        res = model_api([3], 10)
-        for key, value in res.items():
-            assert type(key) is list(schema.keys())[0]
-            assert isinstance(value, schema[key])
-
-
-def test_model_output_api_parameters(app):
-    """test_model_output_api_parameters() should test that hours=10 returns 10 rows of data,
-    that setting the reach returns only that reach."""
-    with app.app_context():
-        df = reach_2_model(process_data(df_usgs=get_live_usgs_data(), df_hobolink=get_live_hobolink_data('code_for_boston_export_21d')), 10)
-        row_count = len(df)
-        assert row_count == 10
-        test_reach = model_api([3], 10)
-        test_reach = list(test_reach.values())
-        expected_reach = list(test_reach[2].items())[0][0]
-        assert 'reach_3' == expected_reach
 
 
 def test_max_hours(app):
