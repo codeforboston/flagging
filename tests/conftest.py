aaab44085dc42d6883876abe24f7ef3518576713
--global <xdanielreeves@gmail.com>
2021-02-07 11:45:52 -0500

    docstrings for fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -23,6 +23,9 @@ def app():
 
 @pytest.fixture
 def live_app(app):
+    """This takes the app instance, and temporarily sets `USE_MOCK_DATA` to
+    False. We can use this for tests where `USE_MOCK_DATA` needs to be False.
+    """
     app.config['USE_MOCK_DATA'], old = False, app.config['USE_MOCK_DATA']
     yield app
     app.config['USE_MOCK_DATA'] = old
@@ -30,18 +33,28 @@ def live_app(app):
 
 @pytest.fixture
 def client(app):
-    """A test client for the app."""
+    """A test client for the app. You can think of the test like a web browser;
+    it retrieves data from the website in a similar way that a browser would.
+    """
     return app.test_client()
 
 
 @pytest.fixture(scope='function', autouse=True)
 def cache():
+    """After every test, we want to clear the cache."""
     yield _cache
     _cache.clear()
 
 
 @pytest.fixture(scope='session')
 def _db(app):
+    """See this:
+
+    https://github.com/jeancochrane/pytest-flask-sqlalchemy
+
+    Basically, this '_db' fixture is required for the above extension to work.
+    We use that extension to allow for easy testing of the database.
+    """
     from flagging_site.data.database import db
     yield db
 
