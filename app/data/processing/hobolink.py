007360c381046081f253a0cb3545a9fe4f6d1976
Daniel Reeves <xdanielreeves@gmail.com>
2025-05-03 10:32:34 -0400

    Fix email exports issue with manual pagination, and fix timestamp representation in frontend

--- app/data/processing/hobolink.py
+++ app/data/processing/hobolink.py
@@ -48,42 +48,67 @@ def get_live_hobolink_data(
         end_date = datetime.now(tz=UTC)
     if start_date is None:
         start_date = end_date - timedelta(days=days_ago)
-    res = request_to_hobolink(start_date=start_date, end_date=end_date, loggers=loggers)
-    df = parse_hobolink_data(res.json(), exclude_sensors=exclude_sensors)
+    data = request_to_hobolink(start_date=start_date, end_date=end_date, loggers=loggers)
+    df = parse_hobolink_data(data, exclude_sensors=exclude_sensors)
     return df
 
 
 def request_to_hobolink(
     start_date: datetime, end_date: datetime, loggers: str = None, token: str | None = None
-) -> requests.models.Response:
+) -> list[dict[str, Any]]:
     """ """
     if loggers is None:
         loggers = current_app.config["HOBOLINK_LOGGERS"]
     if token is None:
         token = current_app.config["HOBOLINK_BEARER_TOKEN"]
 
-    res = requests.get(
-        urljoin(BASE_URL, "/v1/data"),
-        params={
-            "start_date_time": start_date.strftime("%Y-%m-%d %H:%M:%S"),
-            "end_date_time": end_date.strftime("%Y-%m-%d %H:%M:%S"),
-            "loggers": loggers,
-        },
-        headers={"Authorization": f"Bearer {token}", "accept": "application/json"},
-    )
+    # HOBOLink API returns max of 100,000 results at a time.
+    # Additionally, there is no pagination.
+    # Therefore we must paginate ourselves.
+    #
+    # Note that API timestamps also pull full closed interval of data,
+    # so we need to be careful to not accidentally pull duplicates by timestamp.
+    pagination_delta = timedelta(days=10)
+    epsilon_delta = timedelta(seconds=1)
+    data: list[dict[str, Any]] = []
 
-    if res.status_code >= 400:
-        error_msg = (
-            f"API request to the HOBOlink endpoint failed with status code {res.status_code}:"
-            + res.text
+    start_date_for_req = start_date
+    end_date_for_req = min(start_date + pagination_delta, end_date)
+    half_interval = False
+
+    while True:
+        res = requests.get(
+            urljoin(BASE_URL, "/v1/data"),
+            params={
+                "start_date_time": start_date_for_req.strftime("%Y-%m-%d %H:%M:%S"),
+                "end_date_time": end_date_for_req.strftime("%Y-%m-%d %H:%M:%S"),
+                "loggers": loggers,
+            },
+            headers={"Authorization": f"Bearer {token}", "accept": "application/json"},
         )
-        abort(500, error_msg)
+        if res.status_code >= 400:
+            error_msg = (
+                f"API request to the HOBOlink endpoint failed with status code {res.status_code}:"
+                + res.text
+            )
+            abort(500, error_msg)
 
-    return res
+        data.extend(res.json()["data"])
+
+        if end_date_for_req == end_date:
+            break
+
+        end_date_for_req += pagination_delta
+        start_date_for_req += pagination_delta
+        if not half_interval:
+            start_date_for_req += epsilon_delta
+            half_interval = True
+
+    return data
 
 
 def parse_hobolink_data(
-    data: dict[str, Any], exclude_sensors: list[str] | None = None
+    data: list[dict[str, Any]], exclude_sensors: list[str] | None = None
 ) -> pd.DataFrame:
     """
     Clean the response from the HOBOlink API.
@@ -98,7 +123,7 @@ def parse_hobolink_data(
         # This sensor is for internal temp of device.
         exclude_sensors = current_app.config["HOBOLINK_EXCLUDE_SENSORS"]
 
-    df = pd.DataFrame(data["data"])
+    df = pd.DataFrame(data)
     df = df.loc[~df["sensor_sn"].isin(exclude_sensors), :]
     df["time"] = pd.to_datetime(df["timestamp"])
     df["sensor_measurement_type"] = df["sensor_measurement_type"].str.lower().str.replace(" ", "_")
